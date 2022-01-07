e069e584452a977d16020cbe40e00f4ac41c1365
Tim Kinnane <timkinnane@gmail.com>
2022-01-07 21:20:41 +1100

    chore: ⚙️ change name to cogs

--- src/utils/package.test.ts
+++ src/utils/package.test.ts
@@ -4,13 +4,13 @@ describe('utils > package', () => {
   describe('readPackage', () => {
     it('reads current package json', () => {
       expect(readPackage('./'))
-        .toHaveProperty('name', '@nested-code/configs')
+        .toHaveProperty('name', '@nested-code/cogs')
     })
   })
   describe('requirePackage', () => {
     it('reads current package json', () => {
       expect(requirePackage('./'))
-      .toHaveProperty('name', '@nested-code/configs')
+      .toHaveProperty('name', '@nested-code/cogs')
     })
   })
 })
